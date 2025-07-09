8b503ce2f72bdc6ca46b512ee2f8dac97a364cab
AnaCarinaPP <ancari1989@gmail.com>
2025-07-09 15:42:54 +0200

    Initial commit

---
+++ src/test/java/com/example/happy_travel/HappyTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.happy_travel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyTravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
