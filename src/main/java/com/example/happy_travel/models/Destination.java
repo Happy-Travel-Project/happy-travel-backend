e8dfa52260301637a1dae3b9fcc1bb0b1abc8801
AnaCarinaPP <ancari1989@gmail.com>
2025-07-10 14:01:00 +0200

    feat(Destination): add destination model

---
+++ src/main/java/com/example/happy_travel/models/Destination.java
@@ -0,0 +1,80 @@
+package com.example.happy_travel.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "destinations")
+public class Destination {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 30)
+    private String country;
+
+    @Column(nullable = false, length = 30)
+    private String city;
+
+    @Column(nullable = false)
+    private String image;
+
+    @Column(nullable = false)
+    private String description;
+
+    private User user;
+
+    public Destination() {
+    }
+
+    public Destination(String country, String city, String image, String description, User user) {
+        this.country = country;
+        this.city = city;
+        this.image = image;
+        this.description = description;
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
