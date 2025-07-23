aa73684a1897bdb7089f2bc55d2247eb44c877d3
More-Pe <peraltamorena.va@gmail.com>
2025-07-23 22:44:50 +0200

    test(DestinationControllersTest): addvoid updateDestination_whenBadRequest_returnsBadRequest method

--- src/test/java/com/example/happy_travel/controllers/DestinationControllersTest.java
+++ src/test/java/com/example/happy_travel/controllers/DestinationControllersTest.java
@@ -18,6 +18,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -42,6 +44,7 @@ public class DestinationControllersTest {
     private Destination destination1EntityWithIdUpdated;
     private DestinationResponse destination1ExpectedResponse;
     private DestinationRequest destination1UpdatedRequest;
+    private DestinationRequest destinationBadRequest;
     private Destination destination2Entity;
     private Long notExistentId;
 
@@ -55,22 +58,34 @@ public class DestinationControllersTest {
         destination1EntityWithIdUpdated = new Destination(1L, "A wonderful trip for those who love nature and animals", "Brazil", "Ilha Grande", "https://www.latamairlines.com/content/dam/latamxp/sites/vamos-latam/news-playas-brasil/destino/resize/V26_Brazil_IlhaGrande_VivaLATAM_NevinXavier1.png", "Ilha Grande – which literally translates to Big Island – is a tropical island just off the coast of Rio de Janeiro state and is about 160 kilometers from Copacabana. It is an island defined by its white beaches and clear blue waters. It is also a car-free zone.");
         destination1ExpectedResponse = new DestinationResponse(1L, "A wonderful trip for those who love nature and animals", "Brazil", "Ilha Grande", "https://www.latamairlines.com/content/dam/latamxp/sites/vamos-latam/news-playas-brasil/destino/resize/V26_Brazil_IlhaGrande_VivaLATAM_NevinXavier1.png", "Ilha Grande – which literally translates to Big Island – is a tropical island just off the coast of Rio de Janeiro state and is about 160 kilometers from Copacabana. It is an island defined by its white beaches and clear blue waters. It is also a car-free zone.");
         destination1UpdatedRequest = new DestinationRequest("A wonderful trip for those who love nature and animals", "Brazil", "Ilha Grande", "https://www.latamairlines.com/content/dam/latamxp/sites/vamos-latam/news-playas-brasil/destino/resize/V26_Brazil_IlhaGrande_VivaLATAM_NevinXavier1.png", "Ilha Grande – which literally translates to Big Island – is a tropical island just off the coast of Rio de Janeiro state and is about 160 kilometers from Copacabana. It is an island defined by its white beaches and clear blue waters. It is also a car-free zone.");
+        destinationBadRequest = new DestinationRequest(" ", "a", "", "a", " ");
         destination2Entity = new Destination("The northern lights in Norway", "Norway", "Lofoten", "https://images.lge.com.sg/images/product-placeholder.png", "Our bold claim is that this part of Norway, with its multiple islands, deep fjords and steep mountains, is among the world's most beautiful and interesting places in which to see the northern lights.");
     }
 
     @Test
-    void updateDestination_whenCorrectRequest_returnsDestinationResponse() throws Exception{
+    void updateDestination_whenCorrectRequest_isOk() throws Exception{
     given(destinationService.updateDestination(anyLong(), any(DestinationRequest.class))).willReturn(destination1ExpectedResponse);
 
     String json = objectMapper.writeValueAsString(destination1UpdatedRequest);
 
     mockMvc.perform(put("/destinations/{id}", destination1Id).contentType(MediaType.APPLICATION_JSON).content(json))
             .andExpect(status().isOk())
-            .andExpect(jsonPath("id").value(1))
+            .andExpect(jsonPath("$.id").value(1))
             .andExpect(jsonPath("$.title").value("A wonderful trip for those who love nature and animals"))
             .andExpect(jsonPath("$.country").value("Brazil"))
             .andExpect(jsonPath("$.city").value("Ilha Grande"))
             .andExpect(jsonPath("$.image").value("https://www.latamairlines.com/content/dam/latamxp/sites/vamos-latam/news-playas-brasil/destino/resize/V26_Brazil_IlhaGrande_VivaLATAM_NevinXavier1.png"))
             .andExpect(jsonPath("$.description").value("Ilha Grande – which literally translates to Big Island – is a tropical island just off the coast of Rio de Janeiro state and is about 160 kilometers from Copacabana. It is an island defined by its white beaches and clear blue waters. It is also a car-free zone."));
+    }
+
+    @Test
+    void updateDestination_whenBadRequest_returnsBadRequest() throws Exception{
+
+        String json = objectMapper.writeValueAsString(destinationBadRequest);
+
+        mockMvc.perform(put("/destinations/{id}", destination1Id).contentType(MediaType.APPLICATION_JSON).content(json))
+                .andExpect(status().isBadRequest());
+
+        verify(destinationService, never()).updateDestination(any(Long.class), any(DestinationRequest.class));
     }
 }
