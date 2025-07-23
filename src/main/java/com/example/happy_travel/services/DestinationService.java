f466c0ed528488e98c712bef247bc7f8db0fc226
More-Pe <peraltamorena.va@gmail.com>
2025-07-23 12:57:35 +0200

    feat(DestinationService): add updateDestination method

---
+++ src/main/java/com/example/happy_travel/services/DestinationService.java
@@ -0,0 +1,31 @@
+package com.example.happy_travel.services;
+
+import com.example.happy_travel.dtos.destination.DestinationMapper;
+import com.example.happy_travel.dtos.destination.DestinationRequest;
+import com.example.happy_travel.dtos.destination.DestinationResponse;
+import com.example.happy_travel.exceptions.EntityNotFoundException;
+import com.example.happy_travel.models.Destination;
+import com.example.happy_travel.repositories.DestinationRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DestinationService {
+    public final DestinationRepository destinationRepository;
+
+    public DestinationService(DestinationRepository destinationRepository){
+        this.destinationRepository = destinationRepository;
+    }
+
+    public DestinationResponse updateDestination(Long id, DestinationRequest destinationRequest){
+        Destination updateDestination = destinationRepository.findById(id).orElseThrow(() -> new EntityNotFoundException(Destination.class.getSimpleName(), "id", id.toString()));
+
+        updateDestination.setCountry(destinationRequest.country());
+        updateDestination.setCity(destinationRequest.city());
+        updateDestination.setImage(destinationRequest.image());
+        updateDestination.setDescription(destinationRequest.description());
+
+        Destination newDestination = destinationRepository.save(updateDestination);
+
+        return DestinationMapper.toDto(newDestination);
+    }
+}
