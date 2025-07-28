6b8afd6e100bec331cf9081b7e670deb6102771e
AnaCarinaPP <ancari1989@gmail.com>
2025-07-28 16:14:58 +0200

    feat(DestinationController, data.sql): update DestinationController and fix data.sql

--- src/main/java/com/example/happy_travel/controllers/DestinationController.java
+++ src/main/java/com/example/happy_travel/controllers/DestinationController.java
@@ -21,37 +21,37 @@ public class DestinationController {
     public DestinationController(DestinationService destinationService) {
         this.destinationService = destinationService;
     }
-
+//OK
     @GetMapping("/destinations")
     public ResponseEntity<List<DestinationResponse>> getAllDestinations() {
         List<DestinationResponse> destinations = destinationService.getAllDestinations();
         return new ResponseEntity<>(destinations, HttpStatus.OK);
     }
-
+// revisar por user_id
     @GetMapping("/destinations/{id}")
     public ResponseEntity<DestinationResponse> getDestinationById(@PathVariable Long id) {
         DestinationResponse destinationResponse = destinationService.getDestinationById(id);
         return new ResponseEntity<>(destinationResponse, HttpStatus.OK);
     }
-
+//ok
     @GetMapping("/destinations/title/{title}")
     public ResponseEntity<DestinationResponse> getDestinationByTitle(@PathVariable String title) {
         DestinationResponse destinationResponse = destinationService.getDestinationByTitle(title);
         return new ResponseEntity<>(destinationResponse, HttpStatus.OK);
     }
-
+    //ok
     @GetMapping("/destinations/city/{city}")
     public ResponseEntity<List<DestinationResponse>> getDestinationByCity(@PathVariable String city) {
         List<DestinationResponse> destinationResponse = destinationService.getDestinationByCity(city);
         return new ResponseEntity<>(destinationResponse, HttpStatus.OK);
     }
-
+    //ok
     @GetMapping("/destinations/country/{country}")
     public ResponseEntity<List<DestinationResponse>> getDestinationByCountry(@PathVariable String country) {
         List<DestinationResponse> destinationResponse = destinationService.getDestinationByCountry(country);
         return new ResponseEntity<>(destinationResponse, HttpStatus.OK);
     }
-
+//vincular con user_id autenticado
     @PostMapping("/destinations")
     public ResponseEntity<DestinationResponse> addDestination(@Valid @RequestBody DestinationRequest destinationRequest) {
         DestinationResponse createdDestination = destinationService.addDestination(destinationRequest);
