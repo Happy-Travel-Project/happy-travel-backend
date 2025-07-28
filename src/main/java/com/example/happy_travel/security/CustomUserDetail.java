d1783270255192810e663e0c043f6aadf1432597
More-Pe <peraltamorena.va@gmail.com>
2025-07-28 13:37:17 +0200

    feat(jwt, data.sql): add JwtAuthFilter, JwtAuthServices and data.sql

--- src/main/java/com/example/happy_travel/security/CustomUserDetail.java
+++ src/main/java/com/example/happy_travel/security/CustomUserDetail.java
@@ -4,7 +4,6 @@ import com.example.happy_travel.models.User;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-
 import java.util.Collection;
 import java.util.List;
 
@@ -17,7 +16,7 @@ public class CustomUserDetail implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority(user.getRole()));
+        return List.of(new SimpleGrantedAuthority(user.getRole().getName()));
     }
 
     @Override
