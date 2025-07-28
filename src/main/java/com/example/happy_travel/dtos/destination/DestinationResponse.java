b20d36cd1e1453fb36c8ba3b11cfc814eefbb9de
paolap <paopin2024@gmail.com>
2025-07-28 14:55:21 +0200

    feat(Destination dtos): add user to dtos

--- src/main/java/com/example/happy_travel/dtos/destination/DestinationResponse.java
+++ src/main/java/com/example/happy_travel/dtos/destination/DestinationResponse.java
@@ -1,12 +1,14 @@
 package com.example.happy_travel.dtos.destination;
 
+import com.example.happy_travel.dtos.user.UserResponse;
+
 public record DestinationResponse(
         Long id,
         String title,
         String country,
         String city,
         String image,
-        String description
+        String description,
+        UserResponse user
 ) {
 }
-
